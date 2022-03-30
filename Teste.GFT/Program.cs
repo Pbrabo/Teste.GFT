84102d81cbadfcb39b062be9c88cf101f52ee992
Pedro Henrique Brabo Alves <84054986+Pbrabo@users.noreply.github.com>
2022-03-30 16:41:59 -0300

    Add files via upload

---
+++ Teste.GFT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teste.GFT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
